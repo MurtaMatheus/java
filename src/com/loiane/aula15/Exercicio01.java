97470faf534a3206ea7fd373af0861590a365383
MurtaMatheus <matheus98murta@hotmail.com>
2022-08-21 18:49:11 -0300

    Aula 15 Exercicio01

--- src/com/loiane/aula15/Exercicio01.java
+++ src/com/loiane/aula15/Exercicio01.java
@@ -1,4 +1,17 @@
 package com.loiane.aula15;
+//Faça um Programa que peça dois números e imprima o maior deles.
+
+import java.util.Scanner;
 
 public class Exercicio01 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("----Solicita dois numeros e imprime o maior-----");
+        System.out.println("\n Insira um numero: ");
+        double num1 = scan.nextDouble();
+        System.out.println("\n Insira o segundo numero: ");
+        double num2 = scan.nextDouble();
+
+        System.out.println("O maior numero informado foi: " + (num1 > num2 ? num1 : num2));
+    }
 }
