c218a12da23cdf573fe23f110368a58a501c38f5
MurtaMatheus <matheus98murta@hotmail.com>
2022-08-21 20:00:00 -0300

    Error

--- src/com/loiane/aula15/Exercicio03.java
+++ src/com/loiane/aula15/Exercicio03.java
@@ -1,4 +1,4 @@
-package com.loiane.aula15;
+package com.loiane.cursojava.aula15;
 //Faça um Programa que verifique se uma letra digitada é "F" ou "M".
 //Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
 
