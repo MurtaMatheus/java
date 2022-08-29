7fdccae5c480a3d91cd71b1f6b94e643befaf0f8
MurtaMatheus <matheus98murta@hotmail.com>
2022-08-29 08:33:21 -0300

    Exercicio 05

--- src/com/loiane/aula17/Exercicio05.java
+++ src/com/loiane/aula17/Exercicio05.java
@@ -8,17 +8,45 @@ permita repetir a operação.*/
 public class Exercicio05 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int populacaoA, populacaoB, numeroAnos = 0, txCrescimentoPopulacaoA, txCrescimentoPopulacaoB;
+        double populacaoA, populacaoB, numeroAnos = 0, txCrescimentoPopulacaoA, txCrescimentoPopulacaoB;
+        boolean executaPrograma = true;
+        String escolha = "";
 
-        System.out.println("Informe a populacao do pais A: ");
-        populacaoA = scan.nextInt();
-        System.out.println("Informe a taxa de crecimento da populacao A: ");
-        txCrescimentoPopulacaoA = scan.nextInt();
-        System.out.println("Informe a populacao do pais B: ");
-        populacaoB = scan.nextInt();
-        System.out.println("Informe a taxa de crecimento da populacao B: ");
-        txCrescimentoPopulacaoB = scan.nextInt();
+        do {
+            System.out.println("Informe a populacao do pais A: ");
+            populacaoA = scan.nextInt();
+            System.out.println("Informe a taxa de crescimento da populacao A: ");
+            txCrescimentoPopulacaoA = scan.nextDouble();
+            System.out.println("Informe a populacao do pais B: ");
+            populacaoB = scan.nextInt();
+            System.out.println("Informe a taxa de crescimento da populacao B: ");
+            txCrescimentoPopulacaoB = scan.nextDouble();
+
+            do {
 
 
+                if (populacaoA < populacaoB && txCrescimentoPopulacaoA > txCrescimentoPopulacaoB) {
+                    populacaoA += (populacaoA / 100) * txCrescimentoPopulacaoA;
+                    populacaoB += (populacaoB / 100) * txCrescimentoPopulacaoB;
+                    numeroAnos++;
+                } else {
+                    System.out.println("Impossivel executar o programa");
+                }
+
+
+            } while (populacaoA < populacaoB);
+
+            System.out.println("A populacao do pais A vai ultrapassar a do pais B com " + numeroAnos + " anos.");
+
+
+            System.out.println("Deseja executar o programa novamente? Digite S-sim e N-não");
+            escolha = scan.next();
+            if (escolha.equalsIgnoreCase("S")) {
+                executaPrograma = true;
+            } else executaPrograma = false;
+
+
+        } while (executaPrograma == true);
     }
 }
+
